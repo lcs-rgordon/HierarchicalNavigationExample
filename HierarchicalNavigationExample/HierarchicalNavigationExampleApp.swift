c5e6f721623483d0a5be677e8dc0eab9ac64f835
Russell Gordon <rgordon@lcs.on.ca>
2024-05-23 17:09:39 -0400

    Initial Commit

---
+++ HierarchicalNavigationExample/HierarchicalNavigationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HierarchicalNavigationExampleApp.swift
+//  HierarchicalNavigationExample
+//
+//  Created by Russell Gordon on 2024-05-23.
+//
+
+import SwiftUI
+
+@main
+struct HierarchicalNavigationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
